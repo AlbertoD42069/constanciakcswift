7ce9cc5de61859f3aad15173f0da7d6502a4c6e7
Jesus Alberto Diaz Dominguez <j.albertho123@gmail.com>
2025-01-29 00:23:31 -0600

    agregado de clases iniciales

---
+++ ConstanciasKC/AddStudent/AddStudentUIView.swift
@@ -0,0 +1,20 @@
+//
+//  AddStudentUIView.swift
+//  ConstanciasKC
+//
+//  Created by Jesus Alberto Diaz Dominguez on 29/01/25.
+//
+
+import UIKit
+
+class AddStudentUIView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
